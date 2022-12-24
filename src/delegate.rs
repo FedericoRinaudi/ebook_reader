c1c05682c1f17c42591cedb2a1f795c03f79fc24
aafah <andreaafah@gmail.com>
2022-12-24 11:22:54 +0100

    pre-parallel

--- src/delegate.rs
+++ src/delegate.rs
@@ -1,14 +1,16 @@
-use std::env;
+use std::{env, thread};
+use std::io::sink;
 use std::path::PathBuf;
-use druid::{
-    commands, AppDelegate, AppLauncher, Command, DelegateCtx, Env, FileDialogOptions, FileSpec,
-    Handled, LocalizedString, Target, Widget, WindowDesc,
-};
+use druid::{commands, AppDelegate, AppLauncher, Command, DelegateCtx, Env, FileDialogOptions, FileSpec, Handled, LocalizedString, Target, Widget, WindowDesc, ExtEventSink};
+use druid::im::Vector;
 use druid::piet::TextStorage;
 use crate::algorithms::OcrAlgorithms;
+use crate::app::FINISH_SLOW_FUNCTION;
 use crate::ApplicationState;
+use crate::book::chapter::Chapter;
 use crate::bookcase::BookInfo;
 use crate::utilities::xml_to_text;
+extern crate num_cpus;
 
 
 pub(crate) struct Delegate {}
@@ -16,17 +18,18 @@ pub(crate) struct Delegate {}
 impl AppDelegate<ApplicationState> for Delegate {
     fn command(
         &mut self,
-        _ctx: &mut DelegateCtx,
+        ctx: &mut DelegateCtx,
         _target: Target,
         cmd: &Command,
         data: &mut ApplicationState,
         _env: &Env,
     ) -> Handled {
+
         if let Some(file_info) = cmd.get(commands::SAVE_FILE_AS) {
             let cwd = env::current_dir().unwrap();
             let absolute_path = file_info.path.clone();
 
-            // Strip the prefix of the absolute path that is outside of the project folder
+            // "Normalizziamo" la path: Se fa riferimento a qualcosa nella nostra cartella la teniamo relativa
             let target_path = match absolute_path.clone().strip_prefix(cwd.clone()) {
                 Ok(path) => "./".to_string() + path.to_str().unwrap(),
                 Err(_e) => {
@@ -55,32 +58,66 @@ impl AppDelegate<ApplicationState> for Delegate {
             }
             return Handled::Yes;
         }
+
         if let Some(file_info) = cmd.get(commands::OPEN_FILE) {
             if data.i_mode {
-                let mut lt = leptess::LepTess::new(None, "ita").unwrap();
-                lt.set_image(file_info.path.clone()).unwrap();
-                let text = String::from(lt.get_utf8_text().unwrap().replace("-\n", "")).replace("\n", " ").replace(".", " ");
-
-                for (index, ch) in data.current_book.chapters.iter().enumerate(){
-                    let plain_text = xml_to_text(&ch.xml).replace("\n", " ").replace(".", " ");
-                    let p_clone = plain_text.clone();
-                    let t_clone = text.clone();
-                    if OcrAlgorithms::fuzzy_match(p_clone, t_clone, OcrAlgorithms::fuzzy_linear_compare) {
-                        data.current_book.get_mut_nav().set_ch(index);
-                        data.update_view();
-                        println!("OCR Done");
-                        break;
-                    }
-                }
+                /* Qui stiamo prendendo un immagine per usare l'OCR */
                 data.i_mode = false;
-            } else { println!("epub mode!") }
-            data.is_loading = false;
+
+                th_find_it(ctx.get_external_handle(), file_info.path.clone(), data.current_book.chapters.clone())
+
+            } else {
+                /* Qui stiamo prendendo un epub da aggiungere (?) TODO:IMPLEMENT THIS */
+                println!("epub mode!")
+            }
             return Handled::Yes;
         }
+
+        if let Some(index) = cmd.get(FINISH_SLOW_FUNCTION) {
+            // If the command we received is `FINISH_SLOW_FUNCTION` handle the payload.
+            data.current_book.get_mut_nav().set_ch(index.clone());
+            data.update_view();
+            println!("OCR Done");
+            data.is_loading = false;
+            return Handled::Yes
+        }
+
+
         Handled::No
     }
 }
 
+fn th_find_it(sink: ExtEventSink, path:PathBuf, chs:Vector<Chapter>){
+
+
+
+    thread::spawn(move || {
+        let mut lt = leptess::LepTess::new(None, "ita").unwrap();
+        lt.set_image(path).unwrap();
+        let text = String::from(lt.get_utf8_text().unwrap().replace("-\n", "")).replace("\n", " ").replace(".", " ");
+        let index = find_it(text, chs);
+        // Once the slow function is done we can use the event sink (the external handle).
+        // This sends the `FINISH_SLOW_FUNCTION` command to the main thread and attach
+        // the number as payload.
+        sink.submit_command(FINISH_SLOW_FUNCTION, index, Target::Auto)
+            .expect("command failed to submit");
+    });
+
+}
+
+
+
+fn find_it(text:String, chs:Vector<Chapter>)->usize{
+    for (index, ch) in chs.iter().enumerate(){
+        let plain_text = xml_to_text(&ch.xml).replace("\n", " ").replace(".", " ");
+        let p_clone = plain_text.clone();
+        let t_clone = text.clone();
+        if OcrAlgorithms::fuzzy_match(p_clone, t_clone, OcrAlgorithms::fuzzy_linear_compare) {
+            return index;
+        }
+    }
+    0 //TODO: Use option instead of returning default 0
+}
 
 /* PROVE OCR
 
